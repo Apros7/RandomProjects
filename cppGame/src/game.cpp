6fe6e40ed792f4176d1a66049acf5d4f0bfdbfac
apros7 <lucas.vilsen@gmail.com>
2024-04-26 19:44:31 +0200

    moving player

--- cppGame/src/game.cpp
+++ cppGame/src/game.cpp
@@ -1,5 +1,12 @@
 #include <ncurses.h>
 #include <string>
+#include "game.h"
+#include <unistd.h>
+
+struct {
+    vec2i pos;
+    char disp_char;
+} player;
 
 WINDOW* wnd;
 
@@ -24,21 +31,53 @@ int init() {
     box(wnd, 0, 0);
     attroff(A_BOLD);
 
+    // init_pair(1, COLOR_WHITE, C);
+    // wbkgd(wnd, COLOR_PAIR(1));
+
     return 0;
 }
 
 void run() {
     move(5, 5);
+    player.disp_char = '0';
+    player.pos = (vec2i){10, 5};
 
-    std::string text = "Hello world!";
-    for(int i = 0; i < text.size(); i++) {
-        addch(text[i]);
-        addch(' ');
-    }
+    int in_char;
+    bool exit_requested = false;
 
-    refresh();
+    while(1) {
+        in_char = wgetch(wnd);
+        mvaddch(player.pos.y, player.pos.x, ' ');
+        
+        switch(in_char) {
+            case 'q':
+                exit_requested = true;
+                break;
+            case KEY_UP:
+            case 'w':
+                player.pos.y -= 1;
+                break;
+            case KEY_DOWN:
+            case 's':
+                player.pos.y += 1;
+                break;
+            case KEY_LEFT:
+            case 'a':
+                player.pos.x -= 1;
+                break;
+            case KEY_RIGHT:
+            case 'd':
+                player.pos.x += 1;
+                break;
+            default:
+                break;
+        }
 
-    while(1);
+        mvaddch(player.pos.y, player.pos.x, player.disp_char);
+        usleep(10000);
+        refresh();
+        if(exit_requested) break;
+    };
 }
 
 void close() {
--- cppGame/src/game.h
+++ cppGame/src/game.h
@@ -1,3 +1,13 @@
 int init();
 void run();
 void close();
+
+typedef struct {
+    uint_fast8_t x;
+    uint_fast8_t y;
+} vec2ui;
+
+typedef struct {
+    int_fast8_t x;
+    int_fast8_t y;
+} vec2i;
