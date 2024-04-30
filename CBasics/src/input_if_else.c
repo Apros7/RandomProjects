8d27dd8fc05a433039c53a3f0d71f4aafdebfbd3
apros7 <lucas.vilsen@gmail.com>
2024-04-30 15:07:51 +0200

    if else

--- CBasics/src/input_if_else.c
+++ CBasics/src/input_if_else.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
     char name[6];
     printf("Hello. Please enter your name\n");
     scanf("%5s", name);
-    printf("Hello %s.", name);
+    if (strcmp(name, "Lucas") == 0) {
+        printf("Hello %s.", name);
+    } else {
+        printf("Wrong Name, %s", name);
+    };
     return 0;
 }
+// counterfactuals: what if
