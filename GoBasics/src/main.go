e30ab224f8ff5214a6aa3fc0521961b38f9981d2
apros7 <lucas.vilsen@gmail.com>
2024-05-01 14:56:24 +0200

    go types

--- GoBasics/src/main.go
+++ GoBasics/src/main.go
@@ -1,6 +1,27 @@
 package main
+
 import "fmt"
 
-func main(){
-	fmt.Println("Hello World")	
+func main() {
+	var intNum uint16 = 32767
+	var floatNum float32 = 12345678.9
+	fmt.Println(intNum)
+	fmt.Println(floatNum)
+
+	var intNum2 uint16 = 10000
+	fmt.Println(intNum + intNum2)
+
+	var myString string = "Hello" + " " + "World"
+	fmt.Println(myString)
+
+	const myRune rune = 'a'
+	fmt.Println(myRune)
+
+	int1, int2 := 2, 2
+	printMe(int1 + int2)
+
+}
+
+func printMe(param int) {
+	fmt.Println(param)
 }
