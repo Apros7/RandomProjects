49958f8e0e50fb37ae1e38a82597551b98c3b38a
apros7 <lucas.vilsen@gmail.com>
2024-05-03 11:50:54 +0200

    structs, interfaces and pointers

--- GoBasics/src/structs.go
+++ GoBasics/src/structs.go
@@ -4,6 +4,44 @@ import (
 	"fmt"
 )
 
+type bikeRide struct { // structs are like types in typescript
+	dist     float32
+	hours    float32
+	avgSpeed float32
+}
+
+type swim struct {
+	hundredMeters float32
+	hours         float32
+	avgSpeed      float32
+}
+
+func (r *bikeRide) getAvgSpeed() float32 {
+	r.avgSpeed = r.dist / r.hours
+	return r.dist / r.hours
+}
+
+func (r *swim) getAvgSpeed() float32 {
+	r.avgSpeed = r.hundredMeters / r.hours
+	return r.hundredMeters / r.hours
+
+}
+
+type trip interface {
+	getAvgSpeed() float32
+}
+
+func acceptableSpeed(t trip) {
+	fmt.Println(t.getAvgSpeed())
+	fmt.Println(t)
+	// if r > 20 {
+	// 	fmt.Println("Your speed is good!")
+	// } else {
+	// 	fmt.Println("Go FASTER!!")
+	// }
+}
+
 func structs() {
-	fmt.Println("yo")
+	var myBikeRide *bikeRide = &bikeRide{75, 2.6, 0}
+	acceptableSpeed(myBikeRide)
 }
