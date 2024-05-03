b4d3e7cf3f47371316f0ff74e39986b23dd7663c
apros7 <lucas.vilsen@gmail.com>
2024-05-03 11:08:42 +0200

    strings and runes

--- GoBasics/src/strings.go
+++ GoBasics/src/strings.go
@@ -6,6 +6,13 @@ import (
 
 func strings() {
 	var simpleString string = "This is a string"
-	fmt.Println(simpleString[0]) // Gives 84: Ascii value of "T"
+	fmt.Println(simpleString[0]) // Gives 84: Ascii value of "T", although this can fail if not an ascii character
+	// If we use a for loop, it will give the correct value:
 
+	for i, v := range "résumé" {
+		fmt.Println(i, v)
+	}
+	// or fix it using runes:
+	var runeString = []rune("résumé")
+	fmt.Println(runeString)
 }
---
+++ GoBasics/src/structs.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func structs() {
+	fmt.Println("yo")
+}
