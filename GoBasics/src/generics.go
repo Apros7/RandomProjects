ad403d5882498179f3c189f4009a6a1249add442
apros7 <lucas.vilsen@gmail.com>
2024-05-03 21:55:04 +0200

    generics

--- GoBasics/src/generics.go
+++ GoBasics/src/generics.go
@@ -1,5 +1,21 @@
 package main
 
-func genercis() {
+import (
+	"fmt"
+)
 
+func generics() {
+	var slice []int = []int{1, 2, 3, 4}
+	fmt.Println(sumSlice[int](slice))
+
+	var sliceFloat []float32 = []float32{1, 2, 3, 4}
+	fmt.Println(sumSlice[float32](sliceFloat))
+}
+
+func sumSlice[T int | float32 | float64](slice []T) T {
+	var sum T
+	for _, v := range slice {
+		sum += v
+	}
+	return sum
 }
