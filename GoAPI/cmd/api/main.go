1ba7d7254aed9cec02e23d29b747eea2694dd5e6
apros7 <lucas.vilsen@gmail.com>
2024-05-11 16:44:41 +0200

    introduction message

--- GoAPI/cmd/api/main.go
+++ GoAPI/cmd/api/main.go
@@ -14,7 +14,7 @@ func main() {
 	log.SetReportCaller(true)
 	var r *chi.Mux = chi.NewRouter()
 	handlers.Handler(r)
-	fmt.Println("Starting GO API service...")
+	fmt.Printf("\n--- Starting GO API service ---")
 	fmt.Println(`
 	 ____  ___       _    ____ ___ 
 	/ ___|/ _ \     / \  |  _ \_ _|
@@ -22,7 +22,7 @@ func main() {
        | |_| | |_| |  / ___ \|  __/| | 
 	\____|\___/  /_/   \_\_|  |___|
 	`)
-	fmt.Println("\n --- LIVE ---")
+	fmt.Println("------------- LIVE -------------")
 	err := http.ListenAndServe("localhost:8000", r)
 	if err != nil {
 		log.Error(err)
