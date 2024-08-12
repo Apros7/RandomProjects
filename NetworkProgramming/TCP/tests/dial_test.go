f307a126503ebf40c061e09aab84cc68da1f09a5
apros7 <lucas.vilsen@gmail.com>
2024-08-12 16:25:07 +0200

    dial_test.go

---
+++ NetworkProgramming/TCP/tests/dial_test.go
@@ -0,0 +1,62 @@
+package tests
+
+import (
+	"io"
+	"net"
+	"testing"
+)
+
+func TestDial(t *testing.T) {
+	var listener net.Listener
+	var err error
+
+	listener, err = net.Listen("tcp", "127.0.0.1:")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	done := make(chan struct{})
+	go func() {
+		defer func() { done <- struct{}{} }()
+
+		for {
+			conn, err := listener.Accept()
+			if err != nil {
+				// When the TCP connection is closed, err will not be nil. This is not an error,
+				// but as the connection is closed, .Accept() no longer works, so the test shouldn't fail
+				t.Log(err)
+				return
+			}
+
+			go func(c net.Conn) {
+				defer func() {
+					c.Close()
+					done <- struct{}{}
+				}()
+
+				buf := make([]byte, 1024)
+				for {
+					n, err := c.Read(buf)
+					if err != nil {
+						if err != io.EOF {
+							t.Error(err)
+						}
+						return
+					}
+
+					t.Logf("received: %q", buf[:n])
+				}
+			}(conn)
+		}
+	}()
+
+	conn, err := net.Dial("tcp", listener.Addr().String())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	conn.Close()
+	<-done
+	listener.Close()
+	<-done
+}
--- NetworkProgramming/TCP/tests/listen_test.go
+++ NetworkProgramming/TCP/tests/listen_test.go
@@ -1,4 +1,4 @@
-package TCP
+package tests
 
 import (
 	"net"
