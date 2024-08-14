9f363ca2280523c48ffab66e52c10a79cc2b3343
apros7 <lucas.vilsen@gmail.com>
2024-08-14 15:06:42 +0200

    c preprocessor

--- CBasics/src/variables.c
+++ CBasics/src/variables.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+// #define DEBUG // Or use -DDEBUG at compile time
 
 int main() {
     int age = 21;
     char name[6] = "Lucas";
     printf("Age: %d, Name: %s", age, name);
+
+    #ifdef DEBUG
+    printf("\nWE ARE IN DEBUG MODE %s:%d", __FILE__, __LINE__);
+    #endif
 }
